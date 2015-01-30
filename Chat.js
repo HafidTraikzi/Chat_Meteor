d88a1ac81e116db4be6bd4927e520f3535d0b260
HafidTraikzi <hafid.traikzi@gmail.com>
2015-01-30 21:25:01 +0100

    Ajout getElementById(pseudo) et event.preventDefaut().

--- Chat.js
+++ Chat.js
@@ -4,15 +4,16 @@ if (Meteor.isClient) {
   
   Template.body.events({
     "submit .new_message": function (event) {
-
-      var name = "Anonymous";
+      
+      var name = document.getElementById("pseudo");
       var text = event.target.text.value;
 
       if (text != "") {
         Messages.insert({
-          name: name, text: text, time: new Date(),
-        });
-
+          name: name.value, text: text, time: new Date(),
+        });      
+        
+      event.preventDefault();
       event.target.text.value = "";
 
       return false;
