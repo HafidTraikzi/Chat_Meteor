f23a886dd465bcad0c45baf2147c38503aed15e2
HafidTraikzi <hafid.traikzi@gmail.com>
2015-02-17 11:25:27 +0100

    Nettoyage git

--- Chat.js
+++ Chat.js
@@ -7,32 +7,16 @@ if (Meteor.isClient) {
   });
   
   Template.body.events({
-<<<<<<< HEAD
-    "submit .new_message": function(event) {
-=======
     "submit .new_message": function (event) {
->>>>>>> 4c58b8f80f762ada3aa3e4b965d9253b419f48bf
       
       var name = Meteor.user().username;
       var text = event.target.text.value;
       var scroll_down = document.getElementById("scroll");
       
       if (text != "") {
-<<<<<<< HEAD
         Messages.insert({
           name: name, text: text, time: new Date(),
         });
-=======
-        if (name.value == "") {
-          Messages.insert({
-            name: "Anonymous", text: text, time: new Date(),
-          }); 
-        } else {
-            Messages.insert({
-              name: name, text: text, time: new Date(),
-            });
-          }
->>>>>>> 4c58b8f80f762ada3aa3e4b965d9253b419f48bf
       
       event.preventDefault();
       event.target.text.value = "";
@@ -44,16 +28,11 @@ if (Meteor.isClient) {
 	});
 
   Template.body.helpers({
-<<<<<<< HEAD
-    messages: function() {
-=======
     messages: function () {
->>>>>>> 4c58b8f80f762ada3aa3e4b965d9253b419f48bf
       return Messages.find({});
     }
   });
   
-<<<<<<< HEAD
   Template.user.helpers({
 	  users: function() {
 		  return Meteor.users.find({}, {sort: {username: 1}});
@@ -69,29 +48,11 @@ if (Meteor.isClient) {
   
   Template.registerHelper("online", function() {
 	  return "online";
-=======
-  Template.registerHelper("username", function() {
-	  return (Meteor.user() && Meteor.user().statusDefault) || Meteor.user().username;
->>>>>>> 4c58b8f80f762ada3aa3e4b965d9253b419f48bf
-  });
-  
+	)};
 }
 
 if (Meteor.isServer) {
-<<<<<<< HEAD
-  Meteor.startup(function() {
-    Meteor.publish('userPresence', function() {
-  // Example of using a filter to publish only "online" users:
-  var filter = {state: "online"};
-  return UserPresences.find(filter);
-});
-  });
-}
-=======
   Meteor.startup(function () {
     
   });
 }
-
-
->>>>>>> 4c58b8f80f762ada3aa3e4b965d9253b419f48bf
